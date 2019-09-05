3cf5c4e72e94a2f1feffe622a8e7082bd64d0607
Oliver Kent <o.jkent@gmail.com>
2019-09-05 14:45:01 +0100

    Changed static links to be based on $CFG->root

--- block_newsandevents.php
+++ block_newsandevents.php
@@ -6,7 +6,7 @@ class block_newsandevents extends block_base {
     }
 
 	  public function get_content() {
-        global $DB;
+        global $CFG, $DB;
   		if ($this->content !== null) {
   		  return $this->content;
   		}
@@ -18,19 +18,19 @@ class block_newsandevents extends block_base {
                     <div class="mySlides fade">
                       <div class="numbertext">1 / 3</div>
                       <img src="https://www.w3schools.com/howto/img_mountains_wide.jpg" style="width:100%">
-                      <a class="text" href="http://localhost/moodle/mod/forum/discuss.php?d=' . $posts[count($posts)]->id . '">' . $posts[count($posts)]->name . '</a>
+                      <a class="text" href="' . $CFG->wwwroot . '/mod/forum/discuss.php?d=' . $posts[count($posts)]->id . '">' . $posts[count($posts)]->name . '</a>
                     </div>
 
                     <div class="mySlides fade">
                       <div class="numbertext">2 / 3</div>
-                      <img src="https://www.w3schools.com/howto/img_nature_wide.jpg" href="http://localhost/moodle/mod/forum/discuss.php?d=' . $posts[count($posts)-1]->id . '" style="width:100%">
-                      <a class="text"  href="http://localhost/moodle/mod/forum/discuss.php?d=' . $posts[count($posts)-1]->id . '">' . $posts[count($posts)-1]->name . '</a>
+                      <img src="https://www.w3schools.com/howto/img_nature_wide.jpg" style="width:100%">
+                      <a class="text"  href="' . $CFG->wwwroot . '/mod/forum/discuss.php?d=' . $posts[count($posts)-1]->id . '">' . $posts[count($posts)-1]->name . '</a>
                     </div>
 
                     <div class="mySlides fade">
                       <div class="numbertext">3 / 3</div>
-                      <img src="https://www.w3schools.com/howto/img_snow_wide.jpg" href="http://localhost/moodle/mod/forum/discuss.php?d=' . $posts[count($posts)-2]->id . '" style="width:100%">
-                      <a class="text"  href="http://localhost/moodle/mod/forum/discuss.php?d=' . $posts[count($posts)-2]->id . '">' . $posts[count($posts)-2]->name . '</a>
+                      <img src="https://www.w3schools.com/howto/img_snow_wide.jpg" style="width:100%">
+                      <a class="text"  href="' . $CFG->wwwroot . '/mod/forum/discuss.php?d=' . $posts[count($posts)-2]->id . '">' . $posts[count($posts)-2]->name . '</a>
                     </div>
 
                     <!-- Next and previous buttons -->
--- version.php
+++ version.php
@@ -1,4 +1,4 @@
 <?php
 $plugin->component = 'block_newsandevents';  // Recommended since 2.0.2 (MDL-26035). Required since 3.0 (MDL-48494)
-$plugin->version = 2019090500;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->version = 2019090502;  // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires = 2015111603; // YYYYMMDDHH (This is the release version for Moodle 2.0)
