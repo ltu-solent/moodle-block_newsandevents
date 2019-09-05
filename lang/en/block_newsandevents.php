9ff136cfa9cefc9828041ada680136c2c8930131
Oliver Kent <o.jkent@gmail.com>
2019-09-05 10:56:11 +0100

    Initial commit

---
+++ lang/en/block_newsandevents.php
@@ -0,0 +1,5 @@
+<?php
+$string['pluginname'] = 'News and Events';
+$string['newsandevents'] = 'News and Events';
+$string['newsandevents:addinstance'] = 'Add a new News and Events block';
+$string['newsandevents:myaddinstance'] = 'Add a new Library Opening Times block to the My Moodle page';
