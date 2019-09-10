85aed1d4b766437fedd87c38534c39621dc2011b
Oliver Kent <o.jkent@gmail.com>
2019-09-10 10:43:53 +0100

    Added auto image scrolling

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
+  clearTimeout(timer);
   showSlides(slideIndex += n);
 }
 
@@ -12,15 +13,22 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+console.log(n);
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
+
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+
+  timer = setTimeout(function() { showSlides(slideIndex += 1) }, 10000);
+
+
 }
