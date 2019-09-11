8f18b656ffe9216a5a29a3c9c0fa607d4c048cb5
Oliver Kent <o.jkent@gmail.com>
2019-09-11 11:16:07 +0100

    Added links to the forums in the settings

--- settings.php
+++ settings.php
@@ -8,14 +8,14 @@ $settings->add(new admin_setting_heading(
 $settings->add(new admin_setting_configtext(
             'newsandevents/forumidstaff',
             get_string('forumidstaff', 'block_newsandevents'),
-            get_string('forumiddesc', 'block_newsandevents'),
+            get_string('forumiddesc', 'block_newsandevents') . '<br><a href="/mod/forum/view.php?f=' . get_config('newsandevents', 'forumidstaff') . '">Click here to access the selected forum</a>',
             '0'
         ));
 
 $settings->add(new admin_setting_configtext(
             'newsandevents/forumidstudent',
             get_string('forumidstudent', 'block_newsandevents'),
-            get_string('forumiddesc', 'block_newsandevents'),
+            get_string('forumiddesc', 'block_newsandevents') . '<br><a href="/mod/forum/view.php?f=' . get_config('newsandevents', 'forumidstudent') . '">Click here to access the selected forum</a>',
             '0'
         ));
 
