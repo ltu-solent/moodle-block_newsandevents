60547195e128360754bd245e42fee3e3cd592182
Oliver Kent <o.jkent@gmail.com>
2019-09-11 15:12:59 +0100

    Removed image counter

--- block_newsandevents.php
+++ block_newsandevents.php
@@ -48,7 +48,6 @@ class block_newsandevents extends block_base {
       $i = 0;
       foreach ($images as $image) {
           $slides .= '<div class="mySlides fade">
-                  <div class="numbertext">' . ($i + 1) . '/' . count($images) . '</div>
                   <a target="_blank" href="' . $image->message . '"><img src="' . $CFG->wwwroot . '/pluginfile.php/' . $image->ctx_id . '/mod_forum/attachment/'
                   . $image->forum_id . '/' . $image->filename . '" style="width:100%">
                   </a>
