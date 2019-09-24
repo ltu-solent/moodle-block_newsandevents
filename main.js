c18c9d737761f5a314927a9178791b62ab99b88e
Oliver Kent <o.jkent@gmail.com>
2019-09-24 14:47:25 +0100

    Removed console.log

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-console.log(n);
+
   if (n > slides.length) {
     slideIndex = 1;
   }
