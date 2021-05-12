116ba36aa6f6fea18c7087833d499530125eed28
Sarah Cotton <sarah.cotton@solent.ac.uk>
2021-05-12 09:31:24 +0100

    Version bump

--- version.php
+++ version.php
@@ -1,5 +1,5 @@
 <?php
 
-$plugin->version = 2019090504;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->version = 2021051200;  // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires  = 2018112800; // YYYYMMDDHH (This is the release version for Moodle 2.0)
 $plugin->component = 'block_newsandevents';  // Recommended since 2.0.2 (MDL-26035). Required since 3.0 (MDL-48494)
